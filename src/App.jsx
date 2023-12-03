0c744281754eb3140823f9a64744fc0dded59f02
IreneFelice <i.f.buijing@gmail.com>
2023-12-03 15:38:48 +0100

    een begin

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ function App() {
   return (
     <>
         <h1>Fruitmand bezorgservice</h1>
+        <form>
+            <FruitElement
+                <h2>Aardbeien</h2>
+                <button>-</button>
+                <button>+</button>
+            />
+
+        </form>
     </>
   )
 }
